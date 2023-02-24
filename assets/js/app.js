71a5a486980a34ae419f593eeb0820a311389aa1
VikashMaurya10 <vikashmaurya10stp@gmail.com>
2023-02-24 21:52:45 +0530

    add form section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,20 @@
 const ham = document.querySelector("#ham");
 const nav = document.querySelector(".nav_links");
+const talkBtn = document.querySelector("#talk_btn");
+const contactForm = document.querySelector(".contect-form");
+const closeFormBtn = document.querySelector(".btn_close");
+const Body = document.querySelector("#body");
 
 ham.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+
+talkBtn.addEventListener("click", () => {
+  contactForm.classList.add("showContactForm");
+  Body.classList.add("noScroll");
+});
+closeFormBtn.addEventListener("click", () => {
+  contactForm.classList.remove("showContactForm");
+  Body.classList.remove("noScroll");
+
 });
